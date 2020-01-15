b3efbc8e34cbf641141cd89bccd28bb5164fd2b2
wbrink <will.brink55@gmail.com>
2020-01-15 12:55:59 -0800

    fixed weird line breaks where password is generated

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // getting document elements
-var passwordLength = document.getElementById("length");
-var specialCharsCheckBox = document.getElementById("specialCharacters");
-var numbersCheckBox = document.getElementById("numbers");
-var lowerCaseCheckBox = document.getElementById("lowerCase");
-var upperCaseCheckBox = document.getElementById("upperCase");
-var generateButton = document.getElementById("submit"); // submit button
-var message = document.getElementById("message"); // message area
-var passwordGenerator = document.getElementById("password-generator");
+var passwordLength = document.querySelector("#length");
+var specialCharsCheckBox = document.querySelector("#specialCharacters");
+var numbersCheckBox = document.querySelector("#numbers");
+var lowerCaseCheckBox = document.querySelector("#lowerCase");
+var upperCaseCheckBox = document.querySelector("#upperCase");
+var generateButton = document.querySelector("#submit"); // submit button
+var message = document.querySelector("#message"); // message area
+var passwordGenerator = document.querySelector("#password-generator");
+
 
 // character arrays to choose from
 var specialChars = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?",
@@ -59,18 +60,17 @@ function generatePassword(e) {
     charTypes.push(upperCase);
   } 
 
-  for (var i = 0; i < passwordLength.value; i++) {
-    /* have to choose what type of character will be in there
-       for all (passwordLength.value) characters
+
+  /* have to choose what type of character will be in there
+     for all (passwordLength.value) characters
     */
-    console.log(i);
+  for (var i = 0; i < passwordLength.value; i++) {
     // get integer between 0 (inclusive) and charTypes.length(exclusive) to get chartype
     var index = Math.floor(Math.random() * charTypes.length) 
 
     // then get random number for character list to get value and append to password array
     password[i] = charTypes[index][Math.floor(Math.random() * charTypes[index].length)];
-    console.log(password[i]);
-    
+
     // if first character or last character == " " then redo it.
     if (password[0] === " " || password[passwordLength.value - 1] === " ") {
       i = i - 1; 
@@ -78,12 +78,9 @@ function generatePassword(e) {
   }
   
   password = password.join("");
-  // join array together to make a string
-  passwordGenerator.innerText = password;
-  console.log(password);
-
+  passwordGenerator.firstElementChild.value = password; //add password to textarea
+  
 } // end submit click callback
-
 
 
 
